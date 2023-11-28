f127c3326208e6be3045716b6585be71f43c2d86
john becerra <jfbeccera@gmail.com>
2023-11-28 08:52:15 -0500

    solo la carpeta abstraccion

--- Comic.py
+++
@@ -1,6 +0,0 @@
-from Libro import Libro
-
-class Comic(Libro):
-  def __init__(self, autor, titulo, precio, existencias, id):
-    Libro.__init__(self, autor, titulo, precio, existencias, id)
-
--- Novela.py
+++
@@ -1,5 +0,0 @@
-from Libro import Libro
-
-class Novela(Libro):
-  def __init__(self, autor, titulo, precio, existencias, id):
-    Libro.__init__(self, autor, titulo, precio, existencias, id)
